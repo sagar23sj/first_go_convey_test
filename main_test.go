b090fa1a1731a27f5f67ef6fea00c5d49ae9f005
sagar23sj <sagarsonwane23@gmail.com>
2022-07-24 21:58:10 +0530

    Added first goconvey test

---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestIntegerStuff(t *testing.T) {
+	Convey("Given some integer with a starting value", t, func() {
+		x := 1
+
+		Convey("When the integer is incremented", func() {
+			x++
+
+			Convey("The value should be greater by one", func() {
+				So(x, ShouldEqual, 2)
+			})
+		})
+	})
+}
